80962f6790f07aa578fb3c02520e0cf8111c056a
rw9999 <134976895+rw9999@users.noreply.github.com>
2024-09-10 00:06:53 -0700

    Update server socket.c

--- server socket.c
+++ server socket.c
@@ -10,17 +10,17 @@ int main(){
 
     char server_message[256] = "You have reached the server";
 
-    \\ create the server socket
+    // create the server socket
     int server_socket;
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     //deine the sever address
     struct sockaddr_in server_address\;
-    server_address.sinfamily = AF_INET;
+    server_address.sin_family = AF_INET;
     server_address.sin_port = htons(9002);
-    srver_address.sin_addr.s_addr = INAADDR_ANY;
+    server_address.sin_addr.s_addr = INADDR_ANY;
 
-    //bind the socket to our specifeid IP and port
+    //bind the socket to our specified IP and port
     bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
 
     listen(server_socket, 5);
@@ -30,7 +30,7 @@ int main(){
 
     send(client_socket, server_message, sizeof(server_message), 0);
 
-    \\close the socket
+    // close the socket
     close(server_socket);
 
     return 0;
