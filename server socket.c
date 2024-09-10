cffd2be83eb7f90ec02b50eaaa2c645424c8981b
rw9999 <134976895+rw9999@users.noreply.github.com>
2024-09-09 23:44:56 -0700

    Add files via upload

---
+++ server socket.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+int main(){
+
+    char server_message[256] = "You have reached the server";
+
+    \\ create the server socket
+    int server_socket;
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    //deine the sever address
+    struct sockaddr_in server_address\;
+    server_address.sinfamily = AF_INET;
+    server_address.sin_port = htons(9002);
+    srver_address.sin_addr.s_addr = INAADDR_ANY;
+
+    //bind the socket to our specifeid IP and port
+    bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+
+    listen(server_socket, 5);
+
+    int client_socket;
+    client_socket = accept(server_socket, NULL, NULL);
+
+    send(client_socket, server_message, sizeof(server_message), 0);
+
+    \\close the socket
+    close(server_socket);
+
+    return 0;
+}
