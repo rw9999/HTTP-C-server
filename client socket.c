e4a7c43b397e9b9e3018dc5afec6880f21f80d06
rw9999 <134976895+rw9999@users.noreply.github.com>
2024-09-10 00:05:11 -0700

    Update client socket.c

--- client socket.c
+++ client socket.c
@@ -21,11 +21,11 @@ int main() {
     int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
     //check for error with the connectecion
     if (connection_status == -1){
-        printf("There was an error making a connection to the remote socket \n\n")
+        printf("There was an error making a connection to the remote socket \n\n");
     }
 
     // recieve datra from the server
-    cahr server_response[256];
+    char server_response[256];
     recv(network_socket, &server_response, sizeof(server_response), 0);
 
     //print out the servers repsonse
